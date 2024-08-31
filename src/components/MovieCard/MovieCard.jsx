75c903c5afb642ba2510f7b35447260657b86998
Tamerlan <118521890+MohnatiyDemon@users.noreply.github.com>
2024-08-31 20:18:22 +0300

    is working

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,50 +2,73 @@ import { StarOutlined } from '@ant-design/icons'
 import { Card, Flex, Typography } from 'antd'
 import { format } from 'date-fns'
 import { enGB } from 'date-fns/locale'
+import noImage from '../../public/no-image.png'
 import './MovieCard.css'
 
 const { Title, Text, Paragraph } = Typography
-const imgUrl = 'https://image.tmdb.org/t/p/w500/'
+
+const genreMapping = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Science Fiction',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western',
+}
 
 const MovieCard = ({ movieData }) => {
-  const genresData = movieData?.genres?.map((genre) => {
-    return (
-      <Text code key={genre.id}>
-        {genre.name}
-      </Text>
-    )
-  })
+  const { title, overview, release_date, vote_average, poster_path, genre_ids } = movieData
+
+  const imageUrl = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : noImage
+
+  const genresData = genre_ids?.map((genre) => (
+    <Text code key={genre}>
+      {genreMapping[genre]}
+    </Text>
+  ))
+
+  const releaseDate = release_date
+    ? format(new Date(release_date), 'MMMM dd, yyyy', { locale: enGB })
+    : 'Unknown release date'
   return (
-    <Card hoverable>
-      <Flex gap="20px">
-        <Flex>
-          <img src={imgUrl + movieData.poster_path} alt={movieData.original_title} width="250px" />
-        </Flex>
-        <Flex vertical>
-          <Flex justify="space-between" align="center">
-            <Title level={1}>{movieData.original_title}</Title>
-            <Title level={3} className="vote-average">
-              {movieData.vote_average ? movieData.vote_average.toFixed(1) : null}
-            </Title>
-          </Flex>
-          <Text type="secondary">{format(new Date(movieData.release_date), 'MMMM dd, yyyy', { locale: enGB })}</Text>
-          <Flex wrap>{genresData}</Flex>
-          <Paragraph strong ellipsis={{ rows: 9 }}>
-            {movieData.overview}
-          </Paragraph>
+    <>
+      <Card hoverable>
+        <Flex gap="20px">
           <Flex>
-            <StarOutlined />
-            <StarOutlined />
-            <StarOutlined />
-            <StarOutlined />
-            <StarOutlined />
-            <StarOutlined />
-            <StarOutlined />
-            <StarOutlined />
+            <img src={imageUrl} alt={title} width="250px" />
+          </Flex>
+          <Flex vertical gap="10px">
+            <Flex justify="space-between" align="center">
+              <Title level={2}>{title}</Title>
+              <Title level={3} className="vote-average">
+                {vote_average ? vote_average.toFixed(1) : null}
+              </Title>
+            </Flex>
+            <Text type="secondary">{releaseDate}</Text>
+            <Flex wrap>{genresData}</Flex>
+            <Paragraph strong ellipsis={{ rows: 9 }}>
+              {overview}
+            </Paragraph>
+            <Flex>
+              <StarOutlined />
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-    </Card>
+      </Card>
+    </>
   )
 }
 
---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import MovieCard from '../MovieCard/MovieCard'
+
+const MovieList = () => {
+  const [data, setData] = useState(null)
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTliMzM3NTI5NWY1YmEzNWVhZTkwMTVlYTBmMjBjOSIsIm5iZiI6MTcyNTAxMTI2OC43MTAyMTUsInN1YiI6IjY2ZDE5MTNlM2UxYWI0NWNlNWIxNTI5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._HV6mYV4emMyFSy3cdMo_ZH58oQQ2Q4__C3EWZ7nvYg',
+    },
+  }
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=return&include_adult=true&language=en-US&page=1`,
+        options
+      )
+      const data = await response.json()
+      setData(data.results)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      {data ? data.map((movieData) => <MovieCard key={movieData.id} movieData={movieData} />) : <div>Loading...</div>}
+    </>
+  )
+}
+
+export default MovieList
