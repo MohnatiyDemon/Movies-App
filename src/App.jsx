2672093004c866d229b65e26d7f00b751795e5dd
Tamerlan <118521890+MohnatiyDemon@users.noreply.github.com>
2024-09-19 17:40:40 +0300

    tabs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Input, Layout, Pagination } from 'antd'
+import { Input, Layout, Pagination, Tabs } from 'antd'
 import debounce from 'lodash/debounce'
 import { useMemo, useState } from 'react'
 import './App.css'
 import MovieList from './components/MovieList/MovieList'
+import RatedList from './components/RatedList/RatedList'
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(1)
@@ -29,21 +30,38 @@ const App = () => {
     setTotalPages(pages)
   }
 
+  const items = [
+    {
+      key: '1',
+      label: 'Search',
+      children: (
+        <>
+          <Input className="Input" placeholder="Type to search..." onChange={handleInputChange} />
+
+          <MovieList query={query} currentPage={currentPage} onTotalPagesChange={handleTotalPagesChange} />
+
+          <Pagination
+            className="Pagination"
+            align="center"
+            defaultCurrent={1}
+            total={totalPages * 10}
+            current={currentPage}
+            onChange={handlePaginationChange}
+            showSizeChanger={false}
+          />
+        </>
+      ),
+    },
+    {
+      key: '2',
+      label: 'Rated',
+      children: <RatedList />,
+    },
+  ]
+
   return (
     <Layout className="container">
-      <Input className="Input" placeholder="Type to search..." onChange={handleInputChange} />
-
-      <MovieList query={query} currentPage={currentPage} onTotalPagesChange={handleTotalPagesChange} />
-
-      <Pagination
-        className="Pagination"
-        align="center"
-        defaultCurrent={1}
-        total={totalPages * 10}
-        current={currentPage}
-        onChange={handlePaginationChange}
-        showSizeChanger={false}
-      />
+      <Tabs centered items={items} />
     </Layout>
   )
 }
---
+++ src/components/RatedList/RatedList.jsx
@@ -0,0 +1,13 @@
+import { Typography } from 'antd'
+
+const { Text } = Typography
+
+const RatedList = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <Text>Пока нет оцененных фильмов.</Text>
+    </div>
+  )
+}
+
+export default RatedList
